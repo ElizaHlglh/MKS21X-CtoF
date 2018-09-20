fd0b3745916ec7b96147ff9cd207852d10b8fe16
ElizaHlglh <ewu4@stuy.edu>
2018-09-19 21:09:11 -0400

    CtoFTester.java

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,5 +1,13 @@
 public class CtoFTester{
-  public int celsiusToFahrenheit(int C) {
+  public static void main(String[] args) {
+    System.out.println(celsiusToFahrenheit(100));
+  }
+  //Function to change C to F
+  public static int celsiusToFahrenheit(int C) {
     return (C * 9 / 5) + 32;
+  }
+  //Function to turn F to C
+  public static int fahrenheitToCelsius(int F) {
+    return (F - 32) * 5 / 9;
   }
 }
