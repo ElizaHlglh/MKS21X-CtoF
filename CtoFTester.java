89b10bdf36d8c85a1af238f9420c633633cb512f
ElizaHlglh <ewu4@stuy.edu>
2018-09-19 22:10:09 -0400

    CtoFTester.java

--- CtoFTester.java
+++ CtoFTester.java
@@ -2,6 +2,8 @@ public class CtoFTester{
   public static void main(String[] args) {
     System.out.println(celsiusToFahrenheit(100));
     System.out.println(fahrenheitToCelsius(212));
+    System.out.println(IsCelsius(true,100));
+    System.out.println(IsCelsius(false, 212));
   }
   //Function to change C to F
   public static int celsiusToFahrenheit(int C) {
@@ -10,5 +12,10 @@ public class CtoFTester{
   //Function to turn F to C
   public static int fahrenheitToCelsius(int F) {
     return (F - 32) * 5 / 9;
+  }
+  //Main Function: take the number and type, return the opposite.
+  public static int IsCelsius(boolean type, int degree) {
+    if (type) {return celsiusToFahrenheit(degree);}
+    else {return fahrenheitToCelsius(degree);}
   }
 }
